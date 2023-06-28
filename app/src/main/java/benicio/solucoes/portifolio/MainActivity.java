1d3f1c4d0bca506eee99a8b872a5aea3cae41182
benicio <gustavo024dias@gmail.com>
2023-06-28 20:05:46 -0300

    alertDialog para avaliação

--- app/src/main/java/benicio/solucoes/portifolio/MainActivity.java
+++ app/src/main/java/benicio/solucoes/portifolio/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +17,10 @@ import android.view.View;
 import benicio.solucoes.portifolio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SharedPreferences shared;
+    private SharedPreferences.Editor editor;
+    private Dialog dialog;
+
     private ActivityMainBinding vb;
     private MediaPlayer somClick;
     @Override
@@ -22,11 +30,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(vb.getRoot());
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        shared = getSharedPreferences("avaiacao", MODE_PRIVATE);
+        editor = shared.edit();
+
         somClick = MediaPlayer.create(getApplicationContext(), R.raw.sound_click);
         vb.btnConhecer.setOnClickListener(this);
         vb.btnQuemEu.setOnClickListener(this);
         vb.btnCpmoFunfa.setOnClickListener(this);
         vb.btnContato.setOnClickListener(this);
+        configurarAlertDialog();
+
+        if(!shared.getBoolean("isAvalia", false)){
+            dialog.show();
+        }
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -50,5 +66,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             i.putExtra("type", 1);
             startActivity(i);
         }
+    }
+
+    public void configurarAlertDialog(){
+        AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+        b.setTitle("AVALIAR O APP");
+        b.setMessage("considere avaliar o nosso app com 5 estrelas ⭐⭐⭐⭐⭐");
+        b.setCancelable(false);
+        b.setPositiveButton("Ok", (dialog, which) -> {
+            somClick.start();
+            editor.putBoolean("isAvalia", true);
+            editor.commit();
+            String linkApp = "https://play.google.com/store/apps/details?id=benicio.solucoes.portifolio";
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(linkApp)));
+        });
+        b.setNegativeButton("Agora não", (dialog, which) -> {
+            somClick.start();
+            dialog.dismiss();
+        });
+
+        dialog = b.create();
     }
 }
